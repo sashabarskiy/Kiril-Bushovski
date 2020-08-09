d62c4e5f1e7d82bb84fa283ac9909165b6fcbd08
sashabarskyi <Cfif98Cfif>
2020-08-09 21:19:43 +0200

    componente about

---
+++ src/app/pages/about/about.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: [
+      './fonts.googleapis.com.css.family.Muli.css',
+    './fonts.googleapis.com.css.family.Open.Sans.400.700.css',
+    './fonts.googleapis.com.css.family.Raleway.css',
+    './fonts.googleapis.com.css.family.Slabo.27px.css',
+    './about.component.css',
+  ]
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
